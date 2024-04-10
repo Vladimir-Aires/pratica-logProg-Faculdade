2965dee04178fa86551859b7458a8ab083b949ce
Vladimir <vlad.aires05@gmail.com>
2024-04-10 17:43:13 -0300

    Últimas atualizações

--- aula08/signo.js
+++ aula08/signo.js
@@ -1,35 +1,59 @@
 const info = require("prompt-sync")({sigint : true});
 
 let diaNasc = Number(info("Informe o dia que você nasceu: "));
-let mesNasc = String(info("Informe o mês (em palavras) que você nasceu: "));
-mesNasc = mesNasc.toLowerCase();
+let mesNasc = Number(info("Informe o mês que você nasceu: "));
+
 
 let signo;
 
 switch(mesNasc){
-    case "janeiro" || 1:
-        signo = (diaNasc <= 20) ? "Capricórnio" : "Aquário";
+    case  1:
+        signo = (diaNasc <= 19) ? "Capricórnio" : "Aquário";
         break;
-    case "fevereiro":
 
-    case "março":
+    case  2:
+        signo = (diaNasc <= 18) ? "Aquário" : "Peixes";
+        break;
 
-    case "abril":
+    case  3:
+        signo = (diaNasc <= 20) ? "Peixes" : "Áries";
+        break;
 
-    case "maio":
+    case  4:
+        signo = (diaNasc <= 19) ? "Áries" : "Touro";
+        break;
 
-    case "junho":
+    case  5:
+        signo = (diaNasc <= 20) ? "Touro" : "Gêmeos";
+        break;
 
-    case "julho":
+    case  6:
+        signo = (diaNasc <= 20) ? "Gêmeos" : "Câncer";
+        break;
 
-    case "agosto":
+    case  7:
+        signo = (diaNasc <= 22) ? "Câncer" : "Leão";
+        break;
 
-    case "setembro":
+    case  8: 
+        signo = (diaNasc <= 22) ? "Leão" : "Virgem";
+        break;
 
-    case "outubro":
+    case  9:
+        signo = (diaNasc <= 22) ? "Virgem" : "Libra";
+        break;
+
+    case  10:
+        signo = (diaNasc <= 22) ? "Libra" : "Escorpião";
+        break;
+
+    case  11:
+        signo = (diaNasc <= 21) ? "Escorpião" : "Sagitário";
+        break;
+
+    case  12:
+        signo = (diaNasc <= 21) ? "Sagitário" : "Capricórnio";
+        break;
         
-    case "novembro":
-
-    case "dezembro":
-}
+}       
 console.log("Seu signo é: "+signo);
